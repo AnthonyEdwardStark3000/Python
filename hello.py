ada10e8ac119a40322b80e1586d2d06b5ebffd04
AnthonyEdwardStark3000 <90055674+AnthonyEdwardStark3000@users.noreply.github.com>
2021-09-25 23:54:10 +0530

    python grabs my attention

---
+++ hello.py
@@ -0,0 +1,65 @@
+value=3000
+print(value)
+print("another")
+print("Printing name:")
+name="Suresh babu"
+name='vinayaga'
+print(name)
+_test_="300"
+print(_test_)
+print(type(_test_))
+no=10
+print("number",no)
+print(type(no))
+print(id(no))
+number=10.5
+print(number)
+print(int(number))
+no=123.3424343
+print(no)
+print(round(no))
+print(round(no,3))
+# print(len(no))
+
+dummy=0o756;
+print(dummy)
+no=bin(20)
+print(no)
+no=oct(20)
+print(no)
+no=hex(20)
+print(no)
+no=12.232423435455
+print(type(no))
+no=7.7e3
+print(no)
+no=5.5E3
+print(no)
+no1=5 + 3j
+no2=5 + 7j
+print(no1+no2)
+print(no1.real)
+print(no1.imag)
+print(no1.real<=no2.real)
+success=True
+print(success)
+print(type(success))
+
+print(True+True)
+print(True+False)
+print(True*False)
+# print(True/False)
+print(False/True)
+
+name='stark'
+print(name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[-4])
+print(name[-3])
+print(name[-2])
+print(name[-1])
+print(name[-0])
