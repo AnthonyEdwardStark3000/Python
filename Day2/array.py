010fa3d6a2ef96edd1d11629d9869a67fdfb3933
AnthonyEdwardStark3000 <90055674+AnthonyEdwardStark3000@users.noreply.github.com>
2022-01-24 21:40:04 +0530

    Something found about implicit concatenation in python arrays

---
+++ Day2/array.py
@@ -0,0 +1,27 @@
+#Normal array
+names =[
+    "Stark",
+    "Potts",
+    "Rodey",
+    "Happy",
+    "JARVIS"
+]
+print("*****************\tNormal Arrays\t*****************")
+count = len(names)
+print(f"The total number of elements in the array is :{count}")
+#Subsequent array
+numbers =[
+    "one",
+    "two",
+    "three"
+    "four",
+    "five"
+    "six",
+    "seven"
+    "eight",
+    "nine"
+    "ten"
+]
+numbersCount = len(numbers)
+print("*****************\tSubsequent array\t*****************")
+print(f"The number of elements in the array is :{numbersCount}")
