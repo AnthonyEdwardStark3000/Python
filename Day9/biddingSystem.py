2051f64acd16d6eb8731e49f73bce9b0db3361a9
AnthonyEdwardStark3000 <90055674+AnthonyEdwardStark3000@users.noreply.github.com>
2022-02-01 12:14:22 +0530

    Bidding system

---
+++ Day9/biddingSystem.py
@@ -0,0 +1,31 @@
+# Bidding system creation
+import os
+
+print("************ Bidding System ************")
+bidders = {}
+total_bidders = []
+continue_loop = True
+highest_bid = 0
+winner = ""
+bidder_amount = {}
+# clear = lambda: os.system('cls')
+
+while continue_loop:
+    User_name = input("Enter the name : ")
+    User_price = int(input("Enter the bid price : "))
+    bidders = {"name": User_name, "price": User_price}
+    # total_bidders.append(bidders)
+    users = input("Is there any other bidders ? :").lower()
+    if users == "no":
+        continue_loop = False
+    else:
+        os.system('cls')
+# print(total_bidders)
+for bidder in bidders:
+    bid_amount = bidders[bidder]
+    print(bid_amount)
+    # if int(bid_amount) > highest_bid:
+    #     highest_bid = bid_amount
+    #     winner = bidder
+
+print(f"Highest bid amount is {highest_bid} and the winner is {winner}")
