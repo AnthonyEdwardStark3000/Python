444d8c223b4a067d0d4f7538b5eb63eadec47505
AnthonyEdwardStark3000 <90055674+AnthonyEdwardStark3000@users.noreply.github.com>
2022-02-06 22:03:33 +0530

    Debugging the errors is the basic skill for Developing an skill of an Code Developer

---
+++ Day13/Debugging.py
@@ -0,0 +1,59 @@
+############DEBUGGING#####################
+
+# # Describe Problem
+# print("Function which terminates at 19")
+#
+#
+# def my_function():
+#     for i in range(1, 20):
+#         if i == 20:
+#             print("You got it")
+#
+#
+# my_function()
+
+# # Reproduce the Bug
+# from random import randint
+# print("Function that forgets array index value")
+# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(1, 6)
+# # print(dice_num)
+# # dice_num = dice_num-1 //my change
+# print(dice_imgs[dice_num])
+
+# # Play Computer
+# print("Function that completely forgets about the person born during 1994")
+# year = int(input("What's your year of birth?"))
+# if year > 1980 and year < 1994:
+#   print("You are a millenial.")
+# elif year >= 1994: //my change
+# elif year > 1994:
+#   print("You are a Gen Z.")
+
+# # Fix the Errors
+# print("Function that compares string with arithmetic operator")
+# age = input("How old are you?")
+# age = int(age) //my change
+# if age > 18:
+# print("You can drive at age {age}.")
+
+# #Print is Your Friend
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page == int(input("Number of words per page: "))
+# word_per_page = int(input("Number of words per page: ")) // my change
+# total_words = pages * word_per_page
+# print(total_words)
+
+# #Use a Debugger
+# def mutate(a_list):
+#     b_list = []
+#     for item in a_list:
+#         new_item = item * 2
+#         b_list.append(new_item) // my solution
+#     # b_list.append(new_item)
+#     print(b_list)
+
+
+# mutate([1, 2, 3, 5, 8, 13])
