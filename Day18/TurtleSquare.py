fdac556519c1cfcd743831c8ec8d9733bd2d0b2e
AnthonyEdwardStark3000 <90055674+AnthonyEdwardStark3000@users.noreply.github.com>
2022-02-12 21:44:08 +0530

    turtle

--- Day18/TurtleSquare.py
+++ Day18/TurtleSquare.py
@@ -1,20 +1,22 @@
 # Creating square with turtle
 from turtle import Turtle, Screen
+
+
 def square(turtle_color):
-  colour = turtle_color
-  my_turtle.forward(100)
-  my_turtle.color(colour)
-  my_turtle.right(90)
-  my_turtle.color(colour)
+    colour = turtle_color
+    my_turtle.forward(100)
+    my_turtle.color(colour)
+    my_turtle.right(90)
+    my_turtle.color(colour)
 
 
 my_turtle = Turtle()
 my_turtle.shape("turtle")
 my_turtle.color("violet")
 
-colors = ["Red", "blue", "violet","grey"]
+colors = ["Red", "blue", "violet", "grey"]
 for color in colors:
-	square(turtle_color=color)
+    square(turtle_color=color)
 
 screen = Screen()
 screen.exitonclick()
---
+++ Day18/turtleExample.py
@@ -0,0 +1,8 @@
+import turtle as t
+
+t.Turtle()
+t.shape("turtle")
+t.forward(100)
+import heroes
+
+print(heroes.gen())
