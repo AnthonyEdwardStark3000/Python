92183f241dababafacfb6b271d5821bd2ae6df9d
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-10-02 23:15:29 +0530

    Password encryption with hashlib

--- zeroToMastery/password_manager/check_password.py
+++ zeroToMastery/password_manager/check_password.py
@@ -1,6 +1,26 @@
 import requests
+import hashlib
 
-password = 'D56D985300D4B52EB6E189BE006F44F8D23C5EC9'
-url = 'https://api.pwnedpasswords.com/range/'+ password
-res = requests.get(url)
-print(res)
+
+# password = 'D56D985300D4B52EB6E189BE006F44F8D23C5EC9'
+# password = 'D56D9'
+# url = 'https://api.pwnedpasswords.com/range/'+ password
+# res = requests.get(url)
+# print(res)
+
+def request_api(query_character):
+    url = 'https://api.pwnedpasswords.com/range/' + query_character
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error occured {res.status_code}, check the API and try again .')
+    return res.status_code
+
+
+def pawned_api(password):
+    sha_password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    # print(f'The encrypted password is {sha_password}')
+    return sha_password
+
+password_global = 'D56D9'
+pawned_api(password_global)
+request_api(password_global)
